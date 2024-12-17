8d7fc7fb556f7ad762ee6b1424e009e0583be033
rahulGarg-masai <lantern5320@gmail.com>
2024-12-17 23:41:11 +0530

    Add files via upload

---
+++ unit-2/sprint-4/day-1/assignments/13.js
@@ -0,0 +1,9 @@
+let count = 0;
+const loadingInterval = setInterval(() => {
+  console.log("Loading...");
+  count++;
+  if (count === 5) {
+    clearInterval(loadingInterval);
+    console.log("Loaded successfully!");
+  }
+}, 1000);
