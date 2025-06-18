8877f98d5250748cee363150ca62d5c146823fef
Rahul Garg <lantern5320@gmail.com>
2025-06-18 23:24:32 +0530

    q2

---
+++ unit-5/sprint-1/day-4/assignments/Q2/index.js
@@ -0,0 +1,7 @@
+const {factorial} = require('./factorial');
+console.log(factorial(5));
+console.log(factorial(7));
+console.log(factorial(10));
+console.log(factorial(3));
+console.log(factorial(9));
+
