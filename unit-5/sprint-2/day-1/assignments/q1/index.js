7b8ac82ded60e1827f42fab9a5e0e6a0eb6fb1c5
Rahul Garg <lantern5320@gmail.com>
2025-06-19 12:18:57 +0530

    q1

---
+++ unit-5/sprint-2/day-1/assignments/q1/index.js
@@ -0,0 +1,7 @@
+import boxen from 'boxen';
+
+console.log(boxen('I am using my first external module!',{title:'Hurray!',titleAlignment:'center',borderStyle:'classic'}))
+console.log(boxen('I am using my first external module!',{title:'Hurray!',titleAlignment:'center',borderStyle:"singleDouble"}))
+
+
+console.log(boxen('unicorns love rainbows',{title:'Hurray!! ',titleAlignment:'center'}))
