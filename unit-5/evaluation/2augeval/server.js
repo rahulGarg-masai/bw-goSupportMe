b5b29ae9c7ae2fa249214566be52b2cf82b9899a
Rahul Garg <lantern5320@gmail.com>
2025-08-02 20:57:16 +0530

    project setup and schema requirements done

---
+++ unit-5/evaluation/2augeval/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+const orderRoutes = require('./routes/orderRoutes');
+const errorHandler = require('./middleware/errorHandler');
+
+dotenv.config();
+const app=express();
+connectDB();
+app.use(express.json());
+app.use('/products',productRoutes);
+app.use('/users',userRoutes);
+app.use('/orders',orderRoutes)
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT,()=>{
+    console.log(`server running on port ${PORT}`);
+    
+});
